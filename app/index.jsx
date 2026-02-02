9f48bf9a6af052c149c6ae67ba3c3265c824d5c2
ezra-lw <ezra2lopez@gmail.com>
2026-02-02 15:58:13 +0000

    Ver 2 - Added Logo

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image} from 'react-native'
+import Logo from '../assets/img/logo_light.png'
 
 const Home = () => {
   return (
     <View style={styles.container}>
+        <Image source={Logo} style={styles.img}/>
+
       <Text style ={styles.title}>Welcome to</Text>
+
       <Text style={{ marginTop: 10, marginBottom: 30}}>
         Educentral!
     </Text>
+
+    <View style={styles.card}>      
+        <Text>Your one-stop solution for all educational needs.</Text>
+
+    </View>
+    
     </View>
   )
 }
@@ -23,5 +32,14 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         fontWeight: 'bold',
+    },
+    img: {
+        marginVertical: 20,
+    },
+    card:{
+        backgroundColor: '#eee',
+        padding: 20,
+        borderRadius: 5,
+        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
     }
 })
