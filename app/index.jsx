30b7364498b0216092a231d6fa20955976cb38e6
ezra-lw <ezra2lopez@gmail.com>
2026-02-02 15:21:39 +0000

    Ver 1 - Changing route dir and implementing basic text

---
+++ app/index.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text style ={styles.title}>Welcome to</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30}}>
+        Educentral!
+    </Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+})
