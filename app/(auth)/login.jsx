f118dbcc7fa6bc4fb85cc8a1ba95847846033ef7
ezra-lw <ezra2lopez@gmail.com>
2026-02-03 22:38:03 +0000

    Ver 7 - Pressable Components

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,32 +1,44 @@
-import { StyleSheet} from 'react-native'
+import { StyleSheet, Pressable, Text } from 'react-native'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
 
 // themed components
 
 import ThemedView from '../../components/ThemedView'
-import Spacer from '../../components/Spacer'
 import ThemedText from '../../components/ThemedText'
+import ThemedButton from '../../components/ThemedButton'
+import Spacer from '../../components/Spacer'
+
 
 
 const Login = () => {
-  return (
-    <ThemedView style={styles.container}>
 
-        <Spacer />
-        <ThemedText title={true} style={styles.title}>   
-            Login to your account 
-        </ThemedText>
+    const handleSubmit = () => {
+        console.log('login form submitted')
+    }
+    return (
+        <ThemedView style={styles.container}>
 
-
-        <Spacer height={100}/>
-        <Link href='/register'>
-            <ThemedText style={{ textAlign: 'center' }}> 
-                Register instead
+            <Spacer />
+            <ThemedText title={true} style={styles.title}>
+                Login to your account
             </ThemedText>
-        </Link>
 
-    </ThemedView>
-  )
+            <ThemedButton onPress={handleSubmit}>
+                <Text style = {{ color: '#f2f2f2'}}>Login</Text>
+            
+            </ThemedButton>
+
+
+            <Spacer height={100} />
+            <Link href='/register'>
+                <ThemedText style={{ textAlign: 'center' }}>
+                    Register instead
+                </ThemedText>
+            </Link>
+
+        </ThemedView>
+    )
 }
 
 export default Login
@@ -35,11 +47,20 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        
+        alignItems: 'center',
+
     },
     title: {
         textAlign: 'center',
-        fontSize: 18,
+        fontSize: 25,
         marginBottom: 30,
+    },
+    btn: {
+        backgroundColor: Colors.primary,
+        padding: 15,
+        borderRadius: 5,
+    },
+    pressed: {
+        opacity: 0.8,
     },
 })
--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,32 +1,44 @@
-import { StyleSheet} from 'react-native'
+import { StyleSheet, Text } from 'react-native'
 import { Link } from 'expo-router'
 
 // themed components
 
 import ThemedView from '../../components/ThemedView'
-import Spacer from '../../components/Spacer'
 import ThemedText from '../../components/ThemedText'
+import ThemedButton from '../../components/ThemedButton'
+import Spacer from '../../components/Spacer'
+
+
 
 
 const Register = () => {
-  return (
-    <ThemedView style={styles.container}>
-    
-        <Spacer />
-        <ThemedText title={true} style={styles.title}>   
-            Register your EduCentral Account Below
-        </ThemedText>
 
+    const handleSubmit = () => {
+        console.log('register form submitted')
+    }
 
-        <Spacer height={100}/>
-        <Link href='/login'>
-            <ThemedText style={{ textAlign: 'center' }}> 
-                Login instead
+    return (
+        <ThemedView style={styles.container}>
+
+            <Spacer />
+            <ThemedText title={true} style={styles.title}>
+                Register your EduCentral Account Below
             </ThemedText>
-        </Link>
 
-    </ThemedView>
-  )
+            <ThemedButton onPress={handleSubmit}>
+                <Text style={{ color: '#f2f2f2' }}>Register</Text>
+
+            </ThemedButton>
+
+            <Spacer height={100} />
+            <Link href='/login'>
+                <ThemedText style={{ textAlign: 'center' }}>
+                    Login instead
+                </ThemedText>
+            </Link>
+
+        </ThemedView>
+    )
 }
 
 export default Register
@@ -35,11 +47,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        
+        alignItems: 'center',
+
     },
     title: {
         textAlign: 'center',
-        fontSize: 18,
+        fontSize: 25,
         marginBottom: 30,
     },
 })
